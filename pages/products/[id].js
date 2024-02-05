76e50adb1d759720b219e4b7dc10a0e5739075ce
bravoVki <bravovickey884@gmail.com>
2024-02-05 08:47:39 +0545

    commit all changes

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,14 +1,10 @@
-import { useRouter } from "next/router";
 import ProductDetails from "./details";
 import { useParams } from "next/navigation";
 
 const ProductDetail = () => {
-  const router = useRouter();
-  // const { id } = router.query;
   const params = useParams();
   return (
     <div>
-      {/* Pass the id prop to ProductDetails component */}
       <ProductDetails id={params.id} />
     </div>
   );
